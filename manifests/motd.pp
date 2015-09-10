811831a20aaac87e4670f54c555290cbe5c94dda
Paul Anderson <hpcprofessional@gmail.com>
2015-09-10 08:14:24 -0700

    fixed motd mode

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -3,7 +3,7 @@ class helloworld::motd {
   file { '/etc/motd':
   owner   => 'root',
   group   => 'root',
-  mode    => 0644,
+  mode    => '0644',
   content => "Message 2, Electric Boogaloo !\n",
   }
 }
